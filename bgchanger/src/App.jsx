3fa0a23ad87047eb0e44d0adac375af2671e5a7f
rajnish <rajnishkumar550443@gmail.com>
2024-09-24 18:33:20 +0530

    bg changer

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive');
+  return (
+    <div className='w-full h-screen duration-200'
+      style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+            onClick={() => setColor("red")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "red" }}
+          >Read</button>
+          <button
+            onClick={() => setColor("green")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "green" }}
+          >Green</button>
+          <button
+            onClick={() => setColor("blue")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "blue" }}
+          >Blue</button>
+
+          <button
+            onClick={() => setColor("cyan")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "cyan" }}
+          >Cyan</button>
+
+          <button
+            onClick={() => setColor("#7FFF00")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "#7FFF00" }}
+          >Chartreuse</button>
+
+          <button
+            onClick={() => setColor("#FF7F50")}
+            className='outline-none rounded-full py-1 px-4 text-white shadow-sm'
+            style={{ backgroundColor: "#FF7F50" }}
+          >Coral</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/Card'
 function App() {
   return (
     <>
-      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      {/* <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1> */}
       <Card name="Rani" follow="50k" />
       <Card name="Priyanka" follow="500k" />
       <Card name="Puja" follow="80k" />
